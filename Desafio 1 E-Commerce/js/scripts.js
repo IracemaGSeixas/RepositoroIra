a718bd7d3f983f72307c1a3cdc1e2825a2d11962
IracemaGSeixas <87622330+IracemaGSeixas@users.noreply.github.com>
2021-07-18 19:48:09 -0300

    Desafio 1
    Landing Page do desafio do GAMA Academy. Feito esboço após ter feito papel rascunho dos dados para página. Não 100% concluido falta aprender mais.exit

---
+++ Desafio 1 E-Commerce/js/scripts.js
@@ -0,0 +1,31 @@
+alert("Benvindo ao e-NossoCommerce!!!");
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let tel = document.getElementById('tel').value;
+
+    let data = {
+        nome,
+        email,
+        tel,
+    }
+
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+
+    let carregando = `<p>Carregando...</p>`
+    let pronto = `<p>Pronto...</p>`
+
+    content.innerHTML = carregando
+
+    setTimeout(() => { content.innerHTML = pronto }, 1000)
+
+    alert("Você foi Cadastrado!!!");
+})
